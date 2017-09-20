c14188f71035cc7d4495e2e48ed6d7dcc80f8f3e
AVatch <avatchinsky@outlook.com>
2017-09-20 15:29:03 -0400

    fix

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/home/ubuntu/Ask-Where/")
 
 from app import app as application
-from os.path import dirname
+from os.path import join, dirname
 from dotenv import load_dotenv
 load_dotenv(join(dirname(__file__), '.env'))
 
